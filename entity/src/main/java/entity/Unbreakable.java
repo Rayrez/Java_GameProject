8472bf7256647ecc87f2c1e0be1b92fac752d256
Pierrot-la-menace <pierre.garrido@viacesi.fr>
2019-05-24 15:16:32 +0200

    Continuing the implementation of the new model

--- entity/src/main/java/entity/Unbreakable.java
+++ entity/src/main/java/entity/Unbreakable.java
@@ -1,12 +1,12 @@
 package entity;
 
-public abstract class Breakable extends Entity {
+public abstract class Unbreakable extends Entity {
 	
 	private Breakability capacity;
 
-	public Breakable(char symbol, String fileName) {
+	public Unbreakable(char symbol, String fileName) {
 		super(symbol, fileName);
-		this.capacity = Breakability.BREAKABLE;
+		this.capacity = Breakability.UNBREAKABLE;
 	}
 
 	public Breakability getCapacity() {
