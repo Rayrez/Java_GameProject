e93f4e3d04f055d4554bf030d952fc821f980d90
Rayrez13 <benjamin.brifault@hotmail.com>
2019-06-02 23:44:45 +0200

    Debugging Javadoc

--- entity/src/main/java/entity/breakable/Breakable.java
+++ entity/src/main/java/entity/breakable/Breakable.java
@@ -14,7 +14,9 @@ public abstract class Breakable extends Entity {
 	 * Set the capacity Breakable.
 	 *
 	 * @param symbol
+	 * 			The symbol.
 	 * @param fileName
+	 * 			Name of a sprite file.
 	 */
 	public Breakable(char symbol, String fileName) {
 		super(symbol, fileName);
